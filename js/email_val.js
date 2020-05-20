ef21d1e03cb14a51c2fcb2bf205ce2925a298a97
edavid78 <61856383+edavid78@users.noreply.github.com>
2020-05-20 10:38:09 -0400

    Complete

---
+++ js/email_val.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $('#form').trigger("reset");
+    $("form :input").attr("autocomplete", "off");
+    $('#form').submit(function(e) {
+      e.preventDefault();
+      var email = $('#email').val();
+  
+      $(".error").remove();
+  
+
+      if (email.length < 1) {
+        $('#submit').after('<span class="error">Whoops! It looks like you forgot to add your email</span>');
+        $('#email').addClass('error-border');
+        $('#submit').addClass('error-spacing');
+      } else {
+        var regEx = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+        var validEmail = regEx.test(email);
+        if (!validEmail) {
+          $('#submit').after('<span class="error">Please provide a valid email address</span>');
+          $('#email').addClass('error-border');
+          $('#submit').addClass('error-spacing');
+        }else {
+          
+            $('#email').removeClass('error-border');
+            $('#submit').addClass('error-spacing');
+          
+        }
+      }
+      
+    });
+  
+  });
